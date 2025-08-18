7030141451b0d749d9285bd1fc58e14b658835bc
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-08-18 09:51:31 +0000

    Fix: reduced to 5 lines

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 s = "object-oriented programming with Python language"
-print(s[0:15] +
-      s[16:27] +
-      s[28:34] +
-      s[35:41])
+print(s[0:15] + s[16:27] + s[28:34] + s[35:41])
