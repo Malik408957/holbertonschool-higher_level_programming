e6e4e21deab1979e609a2faa6b04c691ad04c911
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-08-18 09:28:19 +0000

    Done task 8 without new variables

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[107:112] + str[:6])
-
+from sys import argv
+print(argv[1][39:66] + argv[1][106:112] + argv[1][:6])
