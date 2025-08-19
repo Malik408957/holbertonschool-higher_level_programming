5ade3c10577df9e5746693d24868661867ec6b2c
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-08-19 09:28:45 +0000

    Fix: no new variable, 5 lines

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str)
 s = "object-oriented programming with Python language"
 print(s[0:19] + s[20:39])
