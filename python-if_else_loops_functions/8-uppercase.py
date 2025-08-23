bb9196f6da856a66f89e1afa9ca72be4c9125b94
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-08-23 02:32:22 +0000

    Add function to print uppercase version of a string

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,6 @@
 def uppercase(str):
     """Convert a string to uppercase"""
     for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-            print("{:c}".format(ord(char) - 32), end="")
-        else:
-            print("{:c}".format(ord(char)), end="")
+        diff = 32 if 97 <= ord(char) <= 122 else 0
+        print("{:c}".format(ord(char) - diff), end="")
     print()
