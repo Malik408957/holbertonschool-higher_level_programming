2e2fa42cc0600f09c8d465d08f1bdded37eb00e6
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-08-24 08:10:09 +0000

    Task 10: Add function to sum two integers

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Return the sum of two integers."""
+    return a + b
+
+
+# Test kodu (opsional)
+if __name__ == "__main__":
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))
