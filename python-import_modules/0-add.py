d46109de8ae9004cb0683ee8b412060c00b52571
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-08-28 08:14:51 +0000

    Add README.md for import & modules task

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-
-# Import the add function from add_0.py
+# Moduldan funksiyanı import et
 from add_0 import add
 
-# Check if this file is being run directly (not imported)
+# Əgər bu fayl birbaşa işə salınırsa (import edilmirsə)
 if __name__ == "__main__":
-    # Assign values to variables
+    # Dəyişənlərə dəyərlər mənimsət
     a = 1
     b = 2
     
-    # Format and print the result
+    # Nəticəni formatla və çap et
     print("{} + {} = {}".format(a, b, add(a, b)))
