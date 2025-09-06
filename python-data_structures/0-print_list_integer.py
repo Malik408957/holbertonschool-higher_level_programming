3487fbcc9f8963c5f3b89bbda73bf96193bfd3f6
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-06 21:27:48 +0000

    Task 0: Print a list of integers

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """
+    Prints all integers of a list, one per line.
+
+    Args:
+        my_list (list): The list of integers to print.
+    """
+    for number in my_list:
+        print("{:d}".format(number))
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
