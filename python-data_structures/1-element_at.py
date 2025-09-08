4bbaf6ee40795ffb1552d794d4b4c5140409f2bf
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-08 05:55:20 +0000

    Completed task 1: Retrieve element from list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    # Check if index is negative or out of range (greater than or equal to list length)
     if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
