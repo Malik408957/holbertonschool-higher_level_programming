1306edc96b6d7b10ebae78a712ee0e2cab13c6b0
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-08 18:13:55 +0000

    Add task 9: find max integer in list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    # Check if the list is empty
+    if len(my_list) == 0:
+        return None 
+    # Initialize max_value with the first element
+    max_value = my_list[0]
+    # Iterate through the list to find the maximum
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+    return max_value
