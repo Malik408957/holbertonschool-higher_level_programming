d4b84549ad8ada242da8bafc6ce76622ee1bd280
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-12 20:18:00 +0000

    Add function to safely print integers from list using try/except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,19 +9,19 @@ def safe_print_list_integers(my_list=[], x=0):
         The real number of integers printed
     """
     count = 0
-    try:
-        for i in range(x):
-            try:
-                # Try to print as integer
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-            except (ValueError, TypeError):
-                # Skip non-integer values silently
-                continue
-    except IndexError:
-        # Re-raise IndexError for out-of-range access
-        raise
-    finally:
-        # Print new line after all elements
-        print()
+    # Xarici try bloku olmadan - IndexError tutulmur
+    for i in range(x):
+        try:
+            # Try to print as integer
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            # Skip non-integer values silently
+            continue
+        except IndexError:
+            # IndexError-i tutmuruq, sadəcə pass edirik və ya raise etmirik
+            # Bu halda for döngüsü təbii olaraq dayanacaq
+            break
+    # Print new line after all elements
+    print()
     return count
