ec846651362a7f9e6460d02c533dbfaff0d0e90d
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-13 08:55:52 +0000

    Add function that raises a TypeError exception

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    Raises a TypeError exception.
+    This function intentionally raises a TypeError to demonstrate
+    exception handling in Python.
+    """
+    raise TypeError
