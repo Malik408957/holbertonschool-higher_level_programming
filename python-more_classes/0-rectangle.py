446b1d0d244e08134f82c520aac56f0af7cf8f3e
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-16 23:44:17 +0000

    Add empty Rectangle class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+0-rectangle module
+Bu modul boş Rectangle sinifini təyin edir.
+"""
+
+
+class Rectangle:
+    """
+    Boş Rectangle sinifi.
+    Bu sinif hal-hazırda heç bir atribut və ya metod əlavə etmir.
+    """
+    pass
