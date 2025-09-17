dd977f4b7537128c7b1444632ee8761db739af39
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-17 11:59:06 +0000

    Add 8-rectangle.py: implement comparison method for bigger or equal rectangle

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""
+8-rectangle module
+Contains Rectangle class with comparison static method
+"""
+
+
+class Rectangle:
+    """Rectangle class with comparison functionality"""
+
+    number_of_instances = 0  # Public class attribute
+    print_symbol = "#"       # Public class attribute for print symbol
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle and increment instance counter"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1  # Increment on creation
+
+    def __del__(self):
+        """Print message on deletion and decrement instance counter"""
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1  # Decrement on deletion
+
+    @property
+    def width(self):
+        """Get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculate and return area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Calculate and return perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return string representation with customizable symbol"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        # Use instance print_symbol if exists, otherwise class print_symbol
+        symbol = getattr(self, 'print_symbol', Rectangle.print_symbol)
+
+        rectangle_str = []
+        for i in range(self.__height):
+            rectangle_str.append(str(symbol) * self.__width)
+        return "\n".join(rectangle_str)
+
+    def __repr__(self):
+        """Return official string representation for eval()"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the bigger rectangle based on area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
