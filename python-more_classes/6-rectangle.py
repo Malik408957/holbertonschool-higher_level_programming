3537c168254ee986bc64a047772e99c5e4d28dd0
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-17 11:12:56 +0000

    Add task 6 with instance counter and del message

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,25 +7,25 @@ Contains Rectangle class with instance counting and deletion message
 
 class Rectangle:
     """Rectangle class with instance counting functionality"""
-    
+
     number_of_instances = 0  # Public class attribute
-    
+
     def __init__(self, width=0, height=0):
         """Initialize rectangle and increment instance counter"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1  # Increment on creation
-    
+
     def __del__(self):
         """Print message on deletion and decrement instance counter"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1  # Decrement on deletion
-    
+
     @property
     def width(self):
         """Get width"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Set width with validation"""
@@ -34,12 +34,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Get height"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set height with validation"""
@@ -48,27 +48,27 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def area(self):
         """Calculate and return area"""
         return self.__width * self.__height
-    
+
     def perimeter(self):
         """Calculate and return perimeter"""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
-    
+
     def __str__(self):
         """Return string representation of rectangle with # characters"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        
+
         rectangle_str = []
         for i in range(self.__height):
             rectangle_str.append("#" * self.__width)
         return "\n".join(rectangle_str)
-    
+
     def __repr__(self):
         """Return official string representation for eval()"""
         return "Rectangle({}, {})".format(self.__width, self.__height)
