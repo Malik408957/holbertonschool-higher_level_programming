3309df8188af4d79382bbb5727726b4b3935c69f
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-20 19:54:24 +0000

    Add function to check if object inherits from a class (not same class)

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ of a class that inherited from a specified class.
 
 def inherits_from(obj, a_class):
     """
-    Check if object is an instance of a class that inherited from specified class
+    Check if object is an instance of a class that inherited.
 
     Args:
         obj: Object to check
@@ -16,4 +16,4 @@ def inherits_from(obj, a_class):
     Returns:
         bool: True if obj is instance of subclass, False otherwise
     """
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
