fd1d9f2dce570ee85906cc224818b4f5c03841ca
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-23 12:57:28 +0000

    Add function to return dictionary for JSON serialization

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,9 +3,10 @@
 Function to return dictionary description for JSON serialization
 """
 
+
 def class_to_json(obj):
     """
-    Returns the dictionary description with simple data structure 
+    Returns the dictionary description with simple data structure
     for JSON serialization of an object.
 
     Args:
