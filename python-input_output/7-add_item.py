54d9aeb420f71893569c9019e58452f8875d9781
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-09-23 11:21:55 +0000

    Add script to load, add, and save arguments to JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,6 +7,7 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
 def main():
     """Main function"""
     filename = "add_item.json"
@@ -17,11 +18,13 @@ def main():
     except FileNotFoundError:
         items = []
 
-    # Add new arguments to the list (skip the first argument which is script name)
+    # Add new arguments to the list
+    # (skip the first argument which is script name)
     items.extend(sys.argv[1:])
 
     # Save the updated list to file
     save_to_json_file(items, filename)
+
 
 if __name__ == "__main__":
     main()
