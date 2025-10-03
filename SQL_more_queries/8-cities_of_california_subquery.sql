f873b9cd8d587d3154c36ae65267172f8f07a5be
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-10-03 20:13:24 +0000

    List cities of California using subquery

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- 8-cities_of_california_subquery.sql
+-- Lists all cities of California using subquery
+
+SELECT id, name 
+FROM cities 
+WHERE state_id = (
+    SELECT id 
+    FROM states 
+    WHERE name = 'California'
+)
+ORDER BY id ASC;
