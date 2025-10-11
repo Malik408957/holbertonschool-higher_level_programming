543648af697744835ac4fa072e4dbdd777a8c858
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-10-11 16:34:07 +0000

    Fix: show actual X-Request-Id or None as required by tests

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -13,9 +13,5 @@ if __name__ == "__main__":
     response = requests.get(url)
     x_request_id = response.headers.get('X-Request-Id')
     
-    # Real X-Request-Id varsa onu göstər
-    if x_request_id:
-        print(x_request_id)
-    else:
-        # Test üçün nümunə X-Request-Id
-        print("5e52e160-c822-4669-8b3a-8b3bbca7b090")
+    # X-Request-Id varsa onu göstər, yoxsa heç nə göstərmə (None)
+    print(x_request_id)
