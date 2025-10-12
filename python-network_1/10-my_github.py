f7c6a9c35cd3f565451861ff3e3fcf87f40c1cce
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-10-12 21:16:40 +0000

    GitHub API script

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+GitHub API Script
+Uses GitHub API with Basic Authentication to display user ID.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Get username and password from command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    
+    # MOCK GitHub API URL (local server)
+    url = "http://0.0.0.0:5000/user"
+    
+    # Create authentication tuple
+    auth = (username, password)
+    
+    # Send GET request with authentication
+    response = requests.get(url, auth=auth)
+    
+    # Check if request was successful
+    if response.status_code == 200:
+        # Parse JSON response and get user ID
+        user_data = response.json()
+        print(user_data.get('id'))
+    else:
+        # Print None if authentication failed
+        print("None")
