e6a3bc7b9bf4b9acfc3aafdaf5ee297fc93b25d8
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-10-12 19:57:17 +0000

    Fix pycodestyle: remove trailing whitespace

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -9,7 +9,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    
     data = {'email': email}
     response = requests.post(url, data=data)
     print(response.text)
