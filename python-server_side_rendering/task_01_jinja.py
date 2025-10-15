ed09f184b74aa4f7b6bacab17632f59de6ebb652
Malik Ağakişiyev <malikaqakisiyev353@gmail.com>
2025-10-15 07:32:57 +0000

    Fix home page content to match test requirements

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -1,11 +1,10 @@
-# task_01_jinja.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Jinja Template Task - Under Construction"
+    return "Welcome to My Flask App"
 
 if __name__ == '__main__':
     app.run(debug=True)
